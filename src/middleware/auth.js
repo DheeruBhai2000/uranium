372a52699f228c1d35be79c366d08f2d768d6e49
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-04-23 07:34:08 +0530

    some cahanges

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,14 @@
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
-
+    try{
+    let token=req.headers["x-auth-token"]
+    if(!token)
     next()
+}catch(err){
+    res.status(400).send(err.message)
 }
-
+}
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
