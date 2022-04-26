a0dabb283947b3e833ee718a8d7c7662b2c615fc
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-04-26 07:05:53 +0530

    apr 25 assignment done

---
+++ src/controllers/mycowincontroller.js
@@ -0,0 +1,100 @@
+const axios = require("axios")
+let getStates = async function (req, res) {
+    try {
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/admin/location/states`
+        }
+        let result = await axios(options)
+        res.status(200).send({ data: result.data })
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+};
+
+let getOtp = async function (req, res) {
+    try {
+        let no = req.body;
+        let options = {
+            method: "post",
+            url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
+            data: no
+        }
+        let result = await axios(options)
+        res.status(200).send({ data: result.data })
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+};
+let getDistricts = async function (req, res) {
+    try {
+        let district_id = req.query.district_id;
+        let date = req.query.date;
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${date}`
+        }
+        let result = await axios(options);
+        res.status(200).send(result.data)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+};
+
+let wather = async function (req, res) {
+    try {
+        // let wether = req.query.q;
+        let key = req.query.key;
+        let newcity = [];
+        let cities = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"];
+        for (let i = 0; i < cities.length; i++) {
+            let obj={city:cities[i]};
+            let options = {
+                method: "get",
+                url: `http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=${key}`
+            }
+            let result = await axios(options);
+            // newcity.city=cities[i]
+            obj.temp=result.data.main.temp
+            newcity.push(obj)
+            // newcity.push(newcity.city=cities[i],newcity.temp=result.data.main.temp)
+        }
+        // console.log(newcity)
+        let sorted=newcity.sort(function(a,b){return a.temp-b.temp})
+        res.status(200).send(sorted)
+
+        // res.status(200).send(result.main.data)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+let meme = async function (req, res) {
+    try {
+        let id = req.query.template_id;
+        let text = req.query.text0;
+        let text2 = req.query.text1
+        let usname = req.query.username;
+        let pass = req.query.password
+        let options = {
+            method: 'post',
+            // method: 'get',
+            url: `https://api.imgflip.com/caption_image?template_id=${id}&text0=${text}&text1=${text2}&username=${usname}&password=${pass}`,
+            data: "hi"
+        }
+        let result = await axios(options);
+        // console.log(result)
+        res.status(200).send(result.data)
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+module.exports.meme = meme
+module.exports.wather = wather
+module.exports.getDistricts = getDistricts
+module.exports.getOtp = getOtp
+module.exports.getStates = getStates
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 // const CowinController= require("../controllers/cowinController")
-
+const cowincontroller=require("../controllers/mycowincontroller")
 
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 // ================ my created apis ===============
-
+router.get("/getstates",cowincontroller.getStates);
+router.post("/getotp",cowincontroller.getOtp);
+router.get("/getdistrict",cowincontroller.getDistricts)
+router.get("/wether",cowincontroller.wather);
+router.get("/meme",cowincontroller.meme)
 
 
 // ===============================================================
